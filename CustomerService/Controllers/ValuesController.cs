7f72ef13e6f7109de637ab14eade2d54dd057e21
Partey, Manish <manish.partey@duckcreek.com>
2020-05-07 20:27:06 +0530

    adding API

---
+++ CustomerService/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CustomerDAL;
+
+namespace CustomerService.Controllers
+{
+	public class ValuesController : ApiController
+	{
+		// GET api/values
+		public IEnumerable<Customer> Get()
+		{
+
+			using (NORTHWNDEntities1 objDB =new NORTHWNDEntities1())
+			{
+				return objDB.Customers.ToList();
+			}
+		}
+
+		// GET api/values/5
+		public Customer Get(string id)
+		{
+			using (NORTHWNDEntities1 objDB = new NORTHWNDEntities1())
+			{
+				return objDB.Customers.FirstOrDefault(e => e.CustomerID == id);
+			}
+		}
+
+		// POST api/values
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/values/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
